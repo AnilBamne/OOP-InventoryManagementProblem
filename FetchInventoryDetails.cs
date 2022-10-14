062e81c135379f153770c81590f85e0a5bb8269d
AnilBamne <99521630+AnilBamne@users.noreply.github.com>
2022-10-14 21:50:18 +0530

    Inventory management problem

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace InventoryManagement
+{
+    public class FetchInventoryDetails
+    {
+        public Inventory Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path)) ;
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Inventory>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InventoryManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "C:/Users/ishwa/source/repos/c#208/InventoryManagement/InventoryManagement/Inventory.json";
+            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
+            Inventory data = fetchInventoryDetails.Read(path);
+            Console.WriteLine(data.typesofRice.name);
+            Console.WriteLine(data.typesofRice.weight);
+            Console.WriteLine(data.typesofRice.price);
+
+        }
+    }
+}
